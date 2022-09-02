5bfaa701420551d3c5f338d2787d5ae2109f5ed0
KI54321 <swoosh.krish@gmail.com>
2022-09-02 19:27:39 -0400

    updates

---
+++ Datability/Datability/DatabilityLogin.swift
@@ -0,0 +1,20 @@
+//
+//  DatabilityLogin.swift
+//  Datability
+//
+//  Created by Krish Iyengar on 9/2/22.
+//
+
+import SwiftUI
+
+struct DatabilityLogin: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct DatabilityLogin_Previews: PreviewProvider {
+    static var previews: some View {
+        DatabilityLogin()
+    }
+}
